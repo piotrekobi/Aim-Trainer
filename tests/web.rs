04e669d437baf47d419d7c78286f7b09380c79cc
kmaj <01143551@pw.edu.pl>
2022-06-07 17:28:31 +0200

    tests repaired

--- tests/web.rs
+++ tests/web.rs
@@ -1,5 +1,6 @@
 use aim_trainer::cursor_inside;
 use aim_trainer::randint;
+use aim_trainer::is_hit;
 use wasm_bindgen_test::wasm_bindgen_test;
 
 #[wasm_bindgen_test]
@@ -14,26 +15,26 @@ pub fn test_cursor_inside() {
 }
 
 #[wasm_bindgen_test]
-pub fn is_hit() {
-    assert_eq!(is_hit(0, 0, 10, 10, 20, 1), true);
+pub fn test_is_hit_1() {
+    assert_eq!(is_hit(0.0, 0.0, 10.0, 10.0, 20.0, 1.0), true);
 }
 
 #[wasm_bindgen_test]
-pub fn is_hit() {
-    assert_eq!(is_hit(10, 10, 0, 0, 20, 1), true);
+pub fn test_is_hit_2() {
+    assert_eq!(is_hit(10.0, 10.0, 0.0, 0.0, 20.0, 1.0), true);
 }
 
 #[wasm_bindgen_test]
-pub fn is_hit() {
-    assert_eq!(is_hit(0, 0, -10, -10, 20, 1), true);
+pub fn test_is_hit_3() {
+    assert_eq!(is_hit(0.0, 0.0, -10.0, -10.0, 20.0, 1.0), true);
 }
 
 #[wasm_bindgen_test]
-pub fn is_hit() {
-    assert_eq!(is_hit(0, 0, 10, 10, 20, 0.5), false);
+pub fn test_is_hit_4() {
+    assert_eq!(is_hit(0.0, 0.0, 10.0, 10.0, 20.0, 0.5), false);
 }
 
 #[wasm_bindgen_test]
-pub fn is_hit() {
-    assert_eq!(is_hit(0, 0, 10, 10, 10, 1), false);
+pub fn test_is_hit_5() {
+    assert_eq!(is_hit(0.0, 0.0, 10.0, 10.0, 10.0, 1.0), false);
 }
